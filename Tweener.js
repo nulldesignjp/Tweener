e6b79221949b2db70e7840f687d0e854fabe8b08
nulldesign <nulldesignjp@users.noreply.github.com>
2021-11-09 08:49:10 +0900

    update props

--- Tweener.js
+++ Tweener.js
@@ -1,290 +1,10 @@
 /*
   Tweener.js
-  ver 2.0.1
+  ver 2.0.2
 */
 
 export default class Tweener{
   constructor(){}
-
-  linear(t,b,c,d)
-  {
-    return c*t/d + b;
-  }
-  
-  //  QUAD
-  easeInQuad(t,b,c,d)
-  {
-    return c*(t/=d)*t + b;
-  }
-  
-  easeOutQuad(t,b,c,d)
-  {
-    return -c *(t/=d)*(t-2) + b;
-  }
-  
-  easeInOutQuad(t,b,c,d)
-  {
-    if ((t/=d/2) < 1) return c/2*t*t + b;
-    return -c/2 * ((--t)*(t-2) - 1) + b;
-  }
-  
-  easeOutInQuad(t,b,c,d)
-  {
-    if (t < d/2) return this.easeOutQuad (t*2, b, c/2, d);
-    return this.easeInQuad((t*2)-d, b+c/2, c/2, d);
-  }
-  
-  //  CUBIC
-  easeInCubic(t,b,c,d)
-  {
-    return c*(t/=d)*t*t + b;
-  }
-  
-  easeOutCubic(t,b,c,d)
-  {
-    return c*((t=t/d-1)*t*t + 1) + b;
-  }
-  
-  easeInOutCubic(t,b,c,d)
-  {
-    if ((t/=d/2) < 1) return c/2*t*t*t + b;
-    return c/2*((t-=2)*t*t + 2) + b;
-  }
-  
-  easeOutInCubic(t,b,c,d)
-  {
-    if (t < d/2) return this.easeOutCubic (t*2, b, c/2, d);
-    return this.easeInCubic((t*2)-d, b+c/2, c/2, d);
-  }
-  
-  //  QUART
-  easeInQuart(t,b,c,d)
-  {
-    return c*(t/=d)*t*t*t + b;
-  }
-  
-  easeOutQuart(t,b,c,d)
-  {
-    return -c * ((t=t/d-1)*t*t*t - 1) + b;
-  }
-  
-  easeInOutQuart(t,b,c,d)
-  {
-    if ((t/=d/2) < 1) return c/2*t*t*t*t + b;
-    return -c/2 * ((t-=2)*t*t*t - 2) + b
-  }
-  
-  easeOutInQuart(t,b,c,d)
-  {
-    if (t < d/2) return this.easeOutQuart (t*2, b, c/2, d);
-    return this.easeInQuart((t*2)-d, b+c/2, c/2, d);
-  }
-  
-  //  QUINT
-  easeInQuint(t,b,c,d)
-  {
-    return c*(t/=d)*t*t*t*t + b;
-  }
-  
-  easeOutQuint(t,b,c,d)
-  {
-    return c*((t=t/d-1)*t*t*t*t + 1) + b;
-  }
-  
-  easeInOutQuint(t,b,c,d)
-  {
-    if ((t/=d/2) < 1) return c/2*t*t*t*t*t + b;
-    return c/2*((t-=2)*t*t*t*t + 2) + b;
-  }
-  
-  easeOutInQuint(t,b,c,d)
-  {
-    if (t < d/2) return this.easeOutQuint (t*2, b, c/2, d);
-    return this.easeInQuint((t*2)-d, b+c/2, c/2, d);
-  }
-  
-  //  SINE
-  easeInSine(t,b,c,d)
-  {
-    return -c * Math.cos(t/d * (Math.PI/2)) + c + b;
-  }
-  
-  easeOutSine(t,b,c,d)
-  {
-    return c * Math.sin(t/d * (Math.PI/2)) + b;
-  }
-  
-  easeInOutSine(t,b,c,d)
-  {
-    return -c/2 * (Math.cos(Math.PI*t/d) - 1) + b;
-  }
-  
-  easeOutInSine(t,b,c,d)
-  {
-    if (t < d/2) return this.easeOutSine (t*2, b, c/2, d);
-    return this.easeInSine((t*2)-d, b+c/2, c/2, d);
-  }
-  
-  //  EXPO
-  easeInExpo(t,b,c,d)
-  {
-    return (t==0) ? b : c * Math.pow(2, 10 * (t/d - 1)) + b - c * 0.001;
-  }
-  
-  easeOutExpo(t,b,c,d)
-  {
-    return (t==d) ? b+c : c * 1.001 * (-Math.pow(2, -10 * t/d) + 1) + b;
-  }
-  
-  easeInOutExpo(t,b,c,d)
-  {
-    if (t==0) return b;
-    if (t==d) return b+c;
-    if ((t/=d/2) < 1) return c/2 * Math.pow(2, 10 * (t - 1)) + b - c * 0.0005;
-    return c/2 * 1.0005 * (-Math.pow(2, -10 * --t) + 2) + b;
-  }
-  
-  easeOutInExpo(t,b,c,d)
-  {
-    if (t < d/2) return this.easeOutExpo (t*2, b, c/2, d);
-    return this.easeInExpo((t*2)-d, b+c/2, c/2, d);
-  }
-  
-  //  CIRC
-  easeInCirc(t,b,c,d)
-  {
-    return -c * (Math.sqrt(1 - (t/=d)*t) - 1) + b;
-  }
-  
-  easeOutCirc(t,b,c,d)
-  {
-    return c * Math.sqrt(1 - (t=t/d-1)*t) + b;
-  }
-  
-  easeInOutCirc(t,b,c,d)
-  {
-    if ((t/=d/2) < 1) return -c/2 * (Math.sqrt(1 - t*t) - 1) + b;
-    return c/2 * (Math.sqrt(1 - (t-=2)*t) + 1) + b;
-  }
-  
-  easeOutInCirc(t,b,c,d)
-  {
-    if (t < d/2) return this.easeOutCirc (t*2, b, c/2, d);
-    return this.easeInCirc((t*2)-d, b+c/2, c/2, d);
-  }
-  
-  //  ELASTIC
-  easeInElastic(t,b,c,d)
-  {
-    if (t==0) return b;
-    if ((t/=d)==1) return b+c;
-    var p = d*.3;
-    var s;
-    var a = 0;
-    if (!Boolean(a) || a < Math.abs(c)) {
-      a = c;
-      s = p/4;
-    } else {
-      s = p/(2*Math.PI) * Math.asin (c/a);
-    }
-    return -(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )) + b;
-  }
-  
-  easeOutElastic(t,b,c,d)
-  {
-    if (t==0) return b;
-    if ((t/=d)==1) return b+c;
-    var p = d*.3;
-    var s;
-    var a = 0;
-    if (!Boolean(a) || a < Math.abs(c)) {
-      a = c;
-      s = p/4;
-    } else {
-      s = p/(2*Math.PI) * Math.asin (c/a);
-    }
-    return (a*Math.pow(2,-10*t) * Math.sin( (t*d-s)*(2*Math.PI)/p ) + c + b);
-  }
-  
-  easeInOutElastic(t,b,c,d)
-  {
-    if (t==0) return b;
-    if ((t/=d/2)==2) return b+c;
-    var p = d*(.3*1.5);
-    var s;
-    var a = 0;
-    if (!Boolean(a) || a < Math.abs(c)) {
-      a = c;
-      s = p/4;
-    } else {
-      s = p/(2*Math.PI) * Math.asin (c/a);
-    }
-    if (t < 1) return -.5*(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )) + b;
-    return a*Math.pow(2,-10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )*.5 + c + b;
-  }
-  
-  easeOutInElastic(t,b,c,d)
-  {
-    if (t < d/2) return this.easeOutElastic (t*2, b, c/2, d);
-    return this.easeInElastic((t*2)-d, b+c/2, c/2, d);
-  }
-  
-  //  BACK
-  easeInBack(t,b,c,d)
-  {
-    var s = 1.70158;
-    return c*(t/=d)*t*((s+1)*t - s) + b;
-  }
-  
-  easeOutBack(t,b,c,d)
-  {
-    var s = 1.70158;
-    return c*((t=t/d-1)*t*((s+1)*t + s) + 1) + b;
-  }
-  
-  easeInOutBack(t,b,c,d)
-  {
-    var s = 1.70158;
-    if ((t/=d/2) < 1) return c/2*(t*t*(((s*=(1.525))+1)*t - s)) + b;
-    return c/2*((t-=2)*t*(((s*=(1.525))+1)*t + s) + 2) + b;
-  }
-  
-  easeOutInBack(t,b,c,d)
-  {
-    if (t < d/2) return this.easeOutBack (t*2, b, c/2, d);
-    return this.easeInBack((t*2)-d, b+c/2, c/2, d);
-  }
-  
-  //  BOUNCE
-  easeInBounce(t,b,c,d)
-  {
-    return c - this.easeOutBounce (d-t, 0, c, d) + b;
-  }
-  
-  easeOutBounce(t,b,c,d)
-  {
-    if ((t/=d) < (1/2.75)) {
-      return c*(7.5625*t*t) + b;
-    } else if (t < (2/2.75)) {
-      return c*(7.5625*(t-=(1.5/2.75))*t + .75) + b;
-    } else if (t < (2.5/2.75)) {
-      return c*(7.5625*(t-=(2.25/2.75))*t + .9375) + b;
-    } else {
-      return c*(7.5625*(t-=(2.625/2.75))*t + .984375) + b;
-    }
-  }
-  
-  easeInOutBounce(t,b,c,d)
-  {
-    if (t < d/2) return this.easeInBounce (t*2, 0, c, d) * .5 + b;
-    else return this.easeOutBounce (t*2-d, 0, c, d) * .5 + c*.5 + b;
-  }
-  
-  easeOutInBounce(t,b,c,d)
-  {
-    if (t < d/2) return this.easeOutBounce (t*2, b, c/2, d);
-    return this.easeInBounce((t*2)-d, b+c/2, c/2, d);
-  }
 }
 
 //  THREE.js only
@@ -323,10 +43,12 @@ Tweener.dispose = () =>
 Tweener.useList = [];
 Tweener.addTween = ( instance, props ) =>
 {
-  props.transition = props.transition == undefined ? linear : props.transition;
+  Tweener.removeTween( instance );
+
+  props.transition = props.transition == undefined ? 'linear' : props.transition;
   props.duration = props.duration == undefined ? 1.0 : props.duration;
   props.delay = props.delay == undefined ? 0.0 : props.delay;
-  props.isPlaying = props.isPlaying == undefined ? true : props.isPlaying
+  props.isPlaying = props.isPlaying == undefined ? true : props.isPlaying;
 
   let _p = {
     instance: instance,
@@ -340,11 +62,18 @@ Tweener.addTween = ( instance, props ) =>
 
   if( props.onComplete ){ _p.onComplete = props.onComplete; }
 
+  //  props
   if( props.position )
   {
     _p.position = {};
     _p.position.start = instance.position.clone();
     _p.position.target = props.position.clone();
+  }
+  if( props.focus )
+  {
+    _p.focus = {};
+    _p.focus.start = instance.focus.clone();
+    _p.focus.target = props.focus.clone();
   }
 
   if( props.rotation )
@@ -373,6 +102,13 @@ Tweener.addTween = ( instance, props ) =>
     _p.opacity = {};
     _p.opacity.start = instance.material.opacity;
     _p.opacity.target = props.opacity;
+  }
+
+  if( props.size && instance.material.size )
+  {
+    _p.size = {};
+    _p.size.start = instance.material.size;
+    _p.size.target = props.size;
   }
 
   Tweener.useList.push( _p );
@@ -476,6 +212,12 @@ Tweener._loop = () =>
       _time = _time < 0.0 ? 0.0 : _time;
 
       //  pos, rot, sca はまとめられないかな？
+      if( _p.focus )
+      {
+        _p.instance.focus.x = _p.transition( _time, _p.focus.start.x, _p.focus.target.x - _p.focus.start.x, _p.duration );
+        _p.instance.focus.y = _p.transition( _time, _p.focus.start.y, _p.focus.target.y - _p.focus.start.y, _p.duration );
+        _p.instance.focus.z = _p.transition( _time, _p.focus.start.z, _p.focus.target.z - _p.focus.start.z, _p.duration );
+      }
       if( _p.position )
       {
         _p.instance.position.x = _p.transition( _time, _p.position.start.x, _p.position.target.x - _p.position.start.x, _p.duration );
@@ -506,11 +248,21 @@ Tweener._loop = () =>
       {
         _p.instance.material.opacity = _p.transition( _time, _p.opacity.start, _p.opacity.target - _p.opacity.start, _p.duration );
       }
+      if( _p.size )
+      {
+        _p.instance.material.size = _p.transition( _time, _p.size.start, _p.size.target - _p.size.start, _p.duration );
+      }
 
     } else {
       Tweener.removeTween( _p.instance );
 
       //  pos, rot, sca はまとめられないかな？
+      if( _p.focus )
+      {
+        _p.instance.focus.x = _p.focus.target.x;
+        _p.instance.focus.y = _p.focus.target.y;
+        _p.instance.focus.z = _p.focus.target.z;
+      }
       if( _p.position )
       {
         _p.instance.position.x = _p.position.target.x;
@@ -540,6 +292,10 @@ Tweener._loop = () =>
       if( _p.opacity )
       {
         _p.instance.material.opacity = _p.opacity.target;
+      }
+      if( _p.size )
+      {
+        _p.instance.material.size = _p.size.target;
       }
 
 
